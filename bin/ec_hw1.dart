c9f0ef6d8c2d352885a9da8e4e42fd1aab6f5fde
moein-keyvani-pur <moein.kpr@gmail.com>
2023-04-14 22:21:13 +0430

    clean RouletteWheel and individual class

--- bin/ec_hw1.dart
+++ bin/ec_hw1.dart
@@ -4,60 +4,67 @@ import 'package:ec_hw1/ec_hw1.dart' as ec_hw1;
 
 void main(List<String> arguments) {
   var primaryPopulation = getPrimaryPopulation(count: 10);
-  for (var element in primaryPopulation) {
-    element.setFitness =
-        evaluationFunction(element) <= 0 ? 0 : evaluationFunction(element);
-    print(element);
-  }
+
   double sumFitness = calculateAllFitness(listIndividual: primaryPopulation);
   for (var element in primaryPopulation) {
     element.setProbabilityChoice = element.getFitness! / sumFitness;
   }
 
-  List<double> sortedAscendingByProbability =
-      primaryPopulation.map((e) => e.getProbabilityChoice!).toList()..sort();
-  sortedAscendingByProbability = sortedAscendingByProbability.reversed.toList();
-  for (var element in sortedAscendingByProbability) {
-    print(element);
-  }
   var selectedPatentsByRouletteWheel = <Individual>[];
   for (var i = 0; i < 10; i++) {
-    double result = RouletteWheel(sortedAscendingByProbability).spin();
-    //! detect which individual was selected by result
-    var selected = primaryPopulation
-        .firstWhere((element) => element.getProbabilityChoice == result);
-    selectedPatentsByRouletteWheel.add(selected);
+    Individual selectedIndividual = RouletteWheel(primaryPopulation).spin();
+
+    selectedPatentsByRouletteWheel.add(selectedIndividual);
   }
-  for (var i = 0; i < 10; i++) {
-    int randomNumber = Random().nextInt(10);
-    var firstParent = selectedPatentsByRouletteWheel[randomNumber];
-    randomNumber = Random().nextInt(10);
-    var secondParent = selectedPatentsByRouletteWheel[randomNumber];
-    int crossoverPointNumber = Random().nextInt(6);
-  }
+  crossover();
 }
 
-// double sumFirstWithIndex(
-//     {required int index, required List<double> sortedList}) {
-//   double sum = 0;
-//   for (var i = 0; i < index; i++) {
-//     sum += sortedList[i];
-//   }
-//   return sum;
-// }
 class RouletteWheel {
-  final List<double> numbers;
+  final List<Individual> individual;
+  late List<Map<String, dynamic>> sortedAscendingByProbability;
+  RouletteWheel(this.individual) {
+    sortedAscendingByProbability = individual
+        .map((e) => {'id': e.getId, 'possibility': e.getProbabilityChoice!})
+        .toList()
+      ..sort(
+        (a, b) =>
+            (a['possibility'] as double).compareTo(b['possibility'] as double),
+      );
+    sortedAscendingByProbability =
+        sortedAscendingByProbability.reversed.toList();
+    for (var element in sortedAscendingByProbability) {
+      print(element);
+    }
+  }
 
-  RouletteWheel(this.numbers);
+  Individual spin() {
+    double randomNumber = Random().nextDouble();
+    Map<String, dynamic> item = _ghaffari(randomNumber: randomNumber);
+    return individual
+        .firstWhere((element) => element.getId == (item['id'] as int));
+  }
 
-  double spin() {
-    double randomNumber = Random().nextDouble() * 100;
+  double _sumFirstUntilIndex({required int index}) {
+    double sum = 0;
+    for (var i = 0; i < index; i++) {
+      sum += (sortedAscendingByProbability[i]['possibility'] as double);
+    }
+    return sum;
+  }
 
-    return numbers[randomNumber.floor()];
+  Map<String, dynamic> _ghaffari({required double randomNumber}) {
+    for (var i = 0; i < sortedAscendingByProbability.length; i++) {
+      if (_sumFirstUntilIndex(index: i) <= randomNumber &&
+          randomNumber < _sumFirstUntilIndex(index: i + 1)) {
+        return sortedAscendingByProbability[i];
+      }
+    }
+    return {};
   }
 }
 
 class Individual {
+  final int _id;
   List<int> _binaryValueA;
   List<int> _binaryValueB;
   List<int> _binaryValueC;
@@ -68,19 +75,22 @@ class Individual {
   double? _probabilityChoice;
 
   Individual({
+    required int id,
     required List<int> binaryValueA,
     required List<int> binaryValueB,
     required List<int> binaryValueC,
     required List<int> binaryValueD,
     required List<int> binaryValueE,
     required List<int> binaryValueF,
-  })  : _binaryValueA = binaryValueA,
+  })  : _id = id,
+        _binaryValueA = binaryValueA,
         _binaryValueB = binaryValueB,
         _binaryValueC = binaryValueC,
         _binaryValueD = binaryValueD,
         _binaryValueE = binaryValueE,
         _binaryValueF = binaryValueF;
 
+  int get getId => _id;
   List<int> get getBinaryValueA => _binaryValueA;
   set setBinaryValueA(List<int> binaryValue) => _binaryValueA = binaryValue;
 
@@ -99,12 +109,31 @@ class Individual {
   List<int> get getBinaryValueF => _binaryValueF;
   set setBinaryValueF(List<int> binaryValue) => _binaryValueF = binaryValue;
 
-  double? get getFitness => _fitness;
-  set setFitness(double? fitness) => _fitness = fitness;
+  double? get getFitness => _evaluationFunction(this);
 
   double? get getProbabilityChoice => _probabilityChoice;
   set setProbabilityChoice(double? probabilityChoice) =>
       _probabilityChoice = probabilityChoice;
+
+  double _evaluationFunction(Individual individual) {
+    var a = individual.getBinaryValueA.binaryToHex();
+    var b = individual.getBinaryValueB.binaryToHex();
+    var c = individual.getBinaryValueC.binaryToHex();
+    var d = individual.getBinaryValueD.binaryToHex();
+    var e = individual.getBinaryValueE.binaryToHex();
+    var f = individual.getBinaryValueF.binaryToHex();
+    var term1 = a * a;
+    var term2 = 0.0;
+    if (a + f != 0) {
+      term2 = (((a * c) - (e * d)) / (a + f)) * b;
+    }
+    var term3 = 0.0;
+    if (e != 0) {
+      term3 = (f * c) / e;
+    }
+    var result = term1 + term2 + term3;
+    return result <= 0 ? 0 : result;
+  }
 
   @override
   String toString() {
@@ -112,23 +141,11 @@ class Individual {
   }
 }
 
-double evaluationFunction(Individual individual) {
-  var a = individual.getBinaryValueA.binaryToHex();
-  var b = individual.getBinaryValueB.binaryToHex();
-  var c = individual.getBinaryValueC.binaryToHex();
-  var d = individual.getBinaryValueD.binaryToHex();
-  var e = individual.getBinaryValueE.binaryToHex();
-  var f = individual.getBinaryValueF.binaryToHex();
-  var term1 = a * a;
-  var term2 = (((a * c) - (e * d)) / (a + f)) * b;
-  var term3 = (f * c) / e;
-  return (term1 + term2 + term3);
-}
-
 List<Individual> getPrimaryPopulation({int count = 10}) {
   var individuals = <Individual>[];
   for (var i = 0; i < count; i++) {
     individuals.add(Individual(
+        id: i + 1,
         binaryValueA: generateRandomBinaryNumber(),
         binaryValueB: generateRandomBinaryNumber(),
         binaryValueC: generateRandomBinaryNumber(),
@@ -172,4 +189,14 @@ double calculateAllFitness({required List<Individual> listIndividual}) {
     sumFitness += element.getFitness!;
   }
   return sumFitness;
+}
+
+void crossover() {
+  // for (var i = 0; i < 10; i++) {
+  //   int randomNumber = Random().nextInt(10);
+  //   var firstParent = selectedPatentsByRouletteWheel[randomNumber];
+  //   randomNumber = Random().nextInt(10);
+  //   var secondParent = selectedPatentsByRouletteWheel[randomNumber];
+  //   int crossoverPointNumber = Random().nextInt(6);
+  // }
 }
